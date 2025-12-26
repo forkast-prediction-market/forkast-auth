d9bd4124331d68ace580eded3e5d7aa45bc2ab69
Bruno Tomé <ibrunotome@gmail.com>
2025-12-26 15:19:02 -0300

    build: bump pino

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   cacheComponents: true,
   typedRoutes: true,
-  serverExternalPackages: ['pino', 'pino-pretty', 'thread-stream'],
   env: {
     CLOB_URL: 'https://clob.forka.st',
     RELAYER_URL: 'https://relayer.forka.st',
